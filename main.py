9a0d337ae51378f182626d9a7023e1b4fca36d38
misbahhassan08 <misbahhassan08@gmail.com>
2024-10-31 14:27:32 +0500

    change docker  file

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def say_hello():
 
  
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=False)
+    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=False)
